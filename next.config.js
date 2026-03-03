be9e68cd2731ae175d040b6f359fccf71758783d
Sarah Haver <shaver@bigw.com>
2026-03-03 13:31:34 +1100

    Demo POC for next/node-config buggy interaction

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const config = require('config');
+
+const { publicRuntimeConfig } = config
+
+const nextConfig = {
+  /* comment out the following line for next to start */
+  publicRuntimeConfig
+};
+
+module.exports = nextConfig
---
+++ pages/index.tsx
@@ -0,0 +1,4 @@
+/** N/A - the bug is in next.config.js parsing not in app code */
+export default function Home() {
+  return null;
+}
